0e317cb1199bb4d7d4f61d95c9cf0cfc106164eb
Soupertoaster <nigeldecontie@gmail.com>
2017-03-04 17:14:33 -0800

    basic snek13

--- app/main.py
+++ app/main.py
@@ -22,16 +22,16 @@ def food_direction(head, food, data):
     y = head[1] - food[1]
     move = None
     if x < 0:
-        if not checkDanger(find_cord('right', head), data):
+        if checkDanger(find_cord('right', head), data):
             move = 'right'
     elif x > 0:
-        if not checkDanger(find_cord('left', head), data):
+        if checkDanger(find_cord('left', head), data):
             move = 'left'
     if y < 0:
-        if not checkDanger(find_cord('down', head), data):
+        if checkDanger(find_cord('down', head), data):
     	    move = 'down'
     elif y > 0:
-        if not checkDanger(find_cord('up', head), data):
+        if checkDanger(find_cord('up', head), data):
     	    move = 'up'
     return move;
 
@@ -43,11 +43,11 @@ def snek_head(data, id):
     return my_snake;
 
 def checkDanger(block, data):
-    x = False;
+    x = True;
     for snake in data['snakes']:
         for coord in snake['coords']:
             if coord == block:
-                x = True
+                x = False
     return x;
 
 def find_cord(str, head):
